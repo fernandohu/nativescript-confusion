a6f67e12c7d09b4bf395f9c942e121f20ef3edac
Uchiyama <fernando.uchiyama@userede.com.br>
2018-05-14 19:56:44 -0300

    Animations and Gestures Part 2

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -11,6 +11,12 @@ import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 import { action } from 'tns-core-modules/ui/dialogs/dialogs';
 import { ModalDialogOptions, ModalDialogService } from 'nativescript-angular/modal-dialog';
 import { CommentComponent } from '~/comment/comment.component';
+import { Page } from "ui/page";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import { Color } from 'color';
+import * as enums from "ui/enums";
 
 @Component({
   selector: 'app-dishdetail',
@@ -27,6 +33,11 @@ export class DishdetailComponent implements OnInit {
   numcomments: number;
   favorite: boolean = false;
   commentHeight: number;
+  showComments: boolean = false;
+
+  cardImage: View;
+  commentList: View;
+  cardLayout: View;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
@@ -35,6 +46,7 @@ export class DishdetailComponent implements OnInit {
     private fonticon: TNSFontIconService,
     private vcRef: ViewContainerRef,
     private modalService: ModalDialogService,
+    private page: Page,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
@@ -54,6 +66,97 @@ export class DishdetailComponent implements OnInit {
         errmess => { this.dish = null; this.errMess = <any>errmess; });
   }
 
+  showAndHideComments() {
+    this.cardImage = <View>this.page.getViewById<View>("cardImage");
+    this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+    this.commentList = <View>this.page.getViewById<View>("commentList");
+
+    if (!this.showComments ) {
+      this.animateUp();
+    }
+    else if (this.showComments ) {
+      this.showComments = false;
+      this.animateDown();
+    }
+  }
+
+  onSwipe(args: SwipeGestureEventData) {
+
+    if (this.dish) {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (args.direction === SwipeDirection.up && !this.showComments ) {
+        this.animateUp();
+      }
+      else if (args.direction === SwipeDirection.down && this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+    }
+
+  }
+
+  animateUp() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 0 },
+        translate: { x: 0, y: -200 },
+        opacity: 0,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffc107"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+      this.showComments = true;
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+  } 
+
+  animateDown() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 1 },
+        translate: { x: 0, y: 0 },
+        opacity: 1,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffffff"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+  } 
+  
   addToFavorites() {
     if (!this.favorite) {
       console.log('Adding to Favorites', this.dish.id);
