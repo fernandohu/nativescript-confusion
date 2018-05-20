ebfb57e22980293f69c3ff596aa667d44a0565d1
Uchiyama <fernando.uchiyama@userede.com.br>
2018-05-20 18:16:33 -0300

    Local notifications

--- app/services/favorite.service.ts
+++ app/services/favorite.service.ts
@@ -4,6 +4,7 @@ import { DishService } from '../services/dish.service';
 import { Observable } from 'rxjs/Observable';
 import { CouchbaseService } from '../services/couchbase.service';
 import 'rxjs/add/operator/map';
+import * as LocalNotifications from 'nativescript-local-notifications';
 
 @Injectable()
 export class FavoriteService {
@@ -31,6 +32,17 @@ export class FavoriteService {
         if (!this.isFavorite(id)) {
             this.favorites.push(id);
             this.couchbaseService.updateDocument(this.docId, {"favorites": this.favorites});
+
+            // Schedule a single notification
+            LocalNotifications.schedule([{
+                id: id,
+                title: "ConFusion Favorites",
+                body: 'Dish ' + id + ' added successfully'
+            }])
+            .then(
+                () => console.log('Notification scheduled'),
+                (error) => console.log('Error showing nofication ' + error)
+            );
         }
     
         return true;
